8ca940ea573b3e5fdf6d686dded4f8585f238008
NickfGe <olavmar2004@gmail.com>
2024-03-10 13:27:06 +0100

    Change between PPT & PTTLS

--- PPT-PPTLS/Assets/Scripts/LogicManager.cs
+++ PPT-PPTLS/Assets/Scripts/LogicManager.cs
@@ -1,18 +1,38 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LogicManager : MonoBehaviour
 {
-    private Toggle pptls;
+    private Toggle toggle;
+    [SerializeField] private TextMeshProUGUI result;
+    public GameObject ppt;
+    public GameObject pptls;
+
 
     private void Awake()
     {
-        pptls = GameObject.Find("Toggle").GetComponent<Toggle>();
+        toggle = GameObject.Find("Toggle").GetComponent<Toggle>();
+        result = GameObject.Find("ResultTxt").GetComponent<TextMeshProUGUI>();
+
+        ChangeModes(true, false);
     }
 
     public void Back()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void EnablePPTLS()
+    {
+        if(toggle.isOn) ChangeModes(false, true);
+        else ChangeModes(true, false);
+    }
+
+    private void ChangeModes(bool normal, bool tbbt)
+    {
+        ppt.SetActive(normal);
+        pptls.SetActive(tbbt);
     }
 }
